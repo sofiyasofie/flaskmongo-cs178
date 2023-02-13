4bfe263ba86a214cf0321afd130b7634d08faeba
Sofiya Lysenko <sofiya.sofie02@gmail.com>
2023-02-13 16:47:08 -0500

    GET method

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ const express = require("express");
 const app = express();
 
 app.listen(3000, () => console.log("Server Up and running"));
+
+app.get('/',(req, res) => {
+    res.send('Hello World!');
+    });
