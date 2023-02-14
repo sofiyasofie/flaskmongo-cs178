fea510df2713c7a666ff0d2ee08c7cce2a43fc76
Sofiya Lysenko <sofiya.sofie02@gmail.com>
2023-02-13 20:40:23 -0500

    Adding concepts

--- index.js
+++ index.js
@@ -13,16 +13,19 @@ app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
+// Concept #1: app get
 app.get('/',(req, res) => {
     res.send('Hello World!');
     });
-
+// Concept #2: app post
 app.post('/',(req, res) => {
     console.log(req.body);
     });
 
+// MongoDB implementation:
 // const dotenv = require(“dotenv”);
 // dotenv.config();
 
+// Concept #3: Listening to a port
 app.listen(3000, () => console.log("Server Up and running"));
 
