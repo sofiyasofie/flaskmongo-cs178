7732eb920223ccbf79078db9bf40498c5be60222
Sofiya Lysenko <sofiya.sofie02@gmail.com>
2023-02-13 20:27:21 -0500

    Mongo

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 Flask Mongo DB to-do app
 
 
-Using this tutorial: https://medium.com/@diogo.fg.pinheiro/simple-to-do-list-app-with-node-js-and-mongodb-chapter-1-c645c7a27583
+Using this tutorial: https://medium.com/@diogo.fg.pinheiro/simple-to-do-list-app-with-node-js-and-mongodb-chapter-1-c645c7a27583 and https://medium.com/@diogo.fg.pinheiro/simple-to-do-list-app-with-node-js-and-mongodb-chapter-2-3780a1c5b039 
 
 Partner (for 2/13/23 helping w debugging in class): Sam Osa-Agbontaen
--- index.js
+++ index.js
@@ -1,8 +1,28 @@
+// Code from https://medium.com/@diogo.fg.pinheiro/simple-to-do-list-app-with-node-js-and-mongodb-chapter-2-3780a1c5b039 and https://medium.com/@diogo.fg.pinheiro/simple-to-do-list-app-with-node-js-and-mongodb-chapter-2-3780a1c5b039
+
 const express = require("express");
 const app = express();
 
-app.listen(3000, () => console.log("Server Up and running"));
+app.use("/static", express.static("public"));
+//https://stackoverflow.com/questions/63542390/problems-getting-css-in-node-js
+// const path = require("path");
+// app.use(express.static(path.join(__dirname, "public")));
+
+app.use(express.urlencoded({ extended: true }));
+
+
+app.set("view engine", "ejs");
 
 app.get('/',(req, res) => {
     res.send('Hello World!');
     });
+
+app.post('/',(req, res) => {
+    console.log(req.body);
+    });
+
+// const dotenv = require(“dotenv”);
+// dotenv.config();
+
+app.listen(3000, () => console.log("Server Up and running"));
+
